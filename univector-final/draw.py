4aa521a00aa786bd3f8fd3e139fe8f0ae2c9297e
Juan Carvalho <juancclcarvalho@gmail.com>
2019-05-07 18:42:15 -0400

    new plot to obstacles

--- univector-final/draw.py
+++ univector-final/draw.py
@@ -13,10 +13,17 @@ def drawVector(field: list, origin: Tuple[int, int], end: Tuple[int, int]) -> No
     cv2.arrowedLine(field, origin, end, (0, 0, 255), 1)
 
 
-def drawPoint(field: list, pos: Tuple[int, int], h: int) -> None:
+def drawObj(field: list, pos: Tuple[int, int], h: int, ball = False) -> None:
 
     x, y = pos
-    cv2.circle(field, toggleY(measures.convertToPixel(x),measures.convertToPixel(y), measures.convertToPixel(h)), 5, (randint(0, 255), randint(0, 255), randint(0, 255)), -1)
+    if ball:
+        cv2.circle(field, toggleY(measures.convertToPixel(x),measures.convertToPixel(y), measures.convertToPixel(h)), 5, (255,255,255), -1)
+    else:
+        cv2.rectangle(field, 
+                      toggleY(measures.convertToPixel(x-2),measures.convertToPixel(y-2), measures.convertToPixel(h)), # Vertex a 
+                      toggleY(measures.convertToPixel(x+2),measures.convertToPixel(y+2), measures.convertToPixel(h)), # Vertex b
+                      (randint(0, 255), randint(0, 255), randint(0, 255)), 
+                      thickness=-1)
 
 
 def drawVectorField(field: list, vectors: List[List[float]], w: int, h: int, step: int, ball: Tuple[int, int], obstacles: List[Tuple[int, int]]) -> list:
@@ -24,10 +31,10 @@ def drawVectorField(field: list, vectors: List[List[float]], w: int, h: int, ste
     length = 10
     k = 0
     if ball is not None:
-        drawPoint(field, ball, h)
+        drawObj(field, ball, h, True)
     if obstacles is not None:
         for obstacle in obstacles:
-            drawPoint(field, obstacle, h)
+            drawObj(field, obstacle, h)
     for x in range(0, w, step):
         for y in range(0, h, step):
             origin = toggleY(measures.convertToPixel(x), measures.convertToPixel(y), measures.convertToPixel(h))
